1f22f668894859d2942b6b981c9004c002ca0a43
dgusfr <diegogustavo.fr@gmail.com>
2025-01-12 16:41:18 -0300

    feat update

--- email_sender.py
+++ email_sender.py
@@ -2,14 +2,6 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from config import EMAIL_REMETENTE, SENHA, EMAIL_DESTINATARIO, URL
-import logging
-
-# Configurar o logging
-logging.basicConfig(
-    filename='preco_monitor.log',
-    level=logging.INFO,
-    format='%(asctime)s:%(levelname)s:%(message)s'
-)
 
 def enviar_email(preco):
     assunto = "Produto Abaixou de Preço!"
@@ -31,8 +23,6 @@ def enviar_email(preco):
             servidor.starttls()
             servidor.login(EMAIL_REMETENTE, SENHA)
             servidor.send_message(mensagem)
-            logging.info("E-mail enviado com sucesso para %s sobre o preço %s.", EMAIL_DESTINATARIO, preco)
             print("E-mail enviado com sucesso!")
     except Exception as e:
-        logging.error("Erro ao enviar o e-mail: %s", e)
         print(f"Erro ao enviar o e-mail: {e}")
