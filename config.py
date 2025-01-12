59098b383e6a53f89b4f58002f020e0ba237186f
dgusfr <diegogustavo.fr@gmail.com>
2025-01-12 16:24:16 -0300

    feat(update)

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
+from dotenv import load_dotenv
+import os
+
+# Carregar variáveis do arquivo .env
+load_dotenv()
+
+# Configurações do e-mail
+EMAIL_REMETENTE = os.getenv("EMAIL_REMETENTE")
+SENHA = os.getenv("SENHA")
+EMAIL_DESTINATARIO = os.getenv("EMAIL_DESTINATARIO")
+
 # Configurações do produto
 URL = "https://www.mercadolivre.com.br/sony-console-playstation-5-digital-slim-branco-1tb-returnal-e-ratchet-e-clank-controle-sem-fio-dualsense-branco/p/MLB37494438"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
 PRECO_ALVO = 3200.00
-
-# Configurações do e-mail
-EMAIL_REMETENTE = "seuemail@gmail.com"  # Substitua pelo seu e-mail
-SENHA = "senha_de_aplicativo"           # Substitua pela senha de aplicativo
-EMAIL_DESTINATARIO = "diegogustavo.fr@gmail.com"
