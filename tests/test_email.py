e7b009a3d73c7134d32b785276ce62f60cb3e089
dgusfr <diegogustavo.fr@gmail.com>
2025-01-12 16:38:00 -0300

    fest add

--- tests/test_email.py
+++ tests/test_email.py
@@ -1,4 +1,9 @@
-from ..email_sender import enviar_email
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from email_sender import enviar_email
 
 if __name__ == "__main__":
     print("Enviando e-mail de teste...")
