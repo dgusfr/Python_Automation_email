1f9413a6ea35bdec899bb3f9eb1ce06b906390c5
dgusfr <diegogustavo.fr@gmail.com>
2025-01-12 16:42:36 -0300

    feat

--- tests/test_email.py
+++ tests/test_email.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-# Adicionar o diretório pai ao PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from email_sender import enviar_email
